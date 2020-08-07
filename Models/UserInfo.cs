63a30a89f0a335a6424533fc62fba9a86636e7a9
PaoloGitAccount <58566249+PaoloGitAccount@users.noreply.github.com>
2020-08-07 15:07:40 +0100

    Add files via upload

---
+++ Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JWTsystem
+{
+    public partial class UserInfo
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Models/UsersDbContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace JWTsystem
+{
+    public partial class UsersDbContext : DbContext
+    {
+        //public UsersDbContext()
+        //{
+        //}
+
+        public UsersDbContext(DbContextOptions<UsersDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+
+        //Note: For security issues, it's good practice to have the Sql server credentials NOT in a C# class.
+        // Moved the ConnectionStrings to the appsettings.json!
+
+        //        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //        {
+        //            if (!optionsBuilder.IsConfigured)
+        //            {
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        //                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=UsersDb;Integrated Security=True");
+        //            }
+        //        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__UserInfo__1788CC4C87F90689");
+
+                entity.HasIndex(e => e.UserId);
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
